7cf87a624e1852eb7afbd845d491fa9fd50ba50b
George Vorobyov <george@oyenetwork.com>
2024-06-07 22:11:40 +0200

    chore: add all-searches component

---
+++ src/app/components/all-searches/all-searches.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-searches',
+  standalone: true,
+  templateUrl: './all-searches.component.html',
+  styleUrls: ['./all-searches.component.scss'],
+})
+export class AllSearchesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
