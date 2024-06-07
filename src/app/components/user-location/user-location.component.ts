782f1c51e7285a67697780a3680d316f19af1c75
George Vorobyov <george@oyenetwork.com>
2024-06-07 22:12:27 +0200

    chore: user-location component

---
+++ src/app/components/user-location/user-location.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-location',
+  standalone: true,
+  templateUrl: './user-location.component.html',
+  styleUrls: ['./user-location.component.scss'],
+})
+export class UserLocationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
