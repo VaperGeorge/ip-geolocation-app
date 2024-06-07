d1b7b14d2fa71e6c36c7f43a2b7a75e9d84974e2
George Vorobyov <george@oyenetwork.com>
2024-06-07 22:11:52 +0200

    chore: add map-location component

---
+++ src/app/components/map-location/map-location.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-location',
+  standalone: true,
+  templateUrl: './map-location.component.html',
+  styleUrls: ['./map-location.component.scss'],
+})
+export class MapLocationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
